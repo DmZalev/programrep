b70e5f3921ab46f05215514f073a682aed446f50
Dmitry <dzalev@gmail.com>
2026-03-11 21:39:54 +0700

    weather API test

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hello, world!!!!!!!!!")
+import requests
+
+base_url = "http://api.openweathermap.org/data/2.5/weather"
+city_name = "London,uk"  # город и код страны
+api_key = "ddd7edd3fcb7f91dbac703663d22145f"  # ваш ключ API
+
+params = {
+    "q": city_name,
+    "appid": api_key
+}
+
+response = requests.get(base_url, params=params)
+
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+else:
+    print("Error:", response.status_code)
